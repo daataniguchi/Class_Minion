8e252fbe320b68c9ae2eacb6bb9efd49ac7ee67f
Jackson Sugar <33731112+jacksonsugar@users.noreply.github.com>
2019-04-12 11:34:56 -0400

    Update Class_Minion.ino

--- Class_Minion/Class_Minion.ino
+++ Class_Minion/Class_Minion.ino
@@ -15,6 +15,7 @@ void setup(void)
   delay(100);
   digitalWrite(Pi_off, LOW);
   
+  // Flash Arduino LED for power conformation
   for(int i = 0; i < 3; i++){
    digitalWrite(LED_BUILTIN, HIGH);
    delay(400);
@@ -30,6 +31,8 @@ void loop(void) {
   delay(500);
   digitalWrite(Pi_on, LOW);
   
+  // Let Pi boot up and take photo
+  
   for (int i = 1; i <= 9; i++){
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
   }
@@ -39,6 +42,8 @@ void loop(void) {
   delay(100);
   digitalWrite(Pi_off, LOW);
 
+  // This is your sample rate set here. Notice the loop is a multiple of the SLEEP_"X"S * i 
+  
   for (int i = 1; i <= 15; i++){
     LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
   }
