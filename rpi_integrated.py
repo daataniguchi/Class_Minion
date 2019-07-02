e86b7cf293418f24eca87ced085b8ffa0e9da156
Guest_3 <baile056@cougars.csusm.edu>
2019-07-02 12:21:39 -0700

    edits in progress

--- rpi_integrated.py
+++ rpi_integrated.py
@@ -1,3 +1,7 @@
+## Use this code for testing in dry conditions, this code will not sudo shutdown now
+## Loops are commented out for now, fr and iso are hard coded for now
+## Editing the picture function to take burst of 5 photos before sleeping to change settings 
+
 import time
 import RPi.GPIO as GPIO 
 from picamera import PiCamera 
@@ -17,56 +21,31 @@ ping_hub = "ping 192.168.0.1 -c 1"
 
 subp = "sudo pkill -9 -f ADXL345_Sampler_100Hz.py"
 
+fps_lst=[15, 20, 25, 30]
+iso_lst=[500, 600, 700, 800]  
 
 
 ##Looping through frame rate:
-fps_top=30
-fps_bottom=15
-fps_increment=15
-fps_lst=[]
-i= fps_bottom
+#fps_top=30
+#fps_bottom=15
+#fps_increment=15
+#fps_lst=[]
+#i= fps_bottom
 
-for i in range (fps_bottom,fps_top+1,fps_increment):
-    fps_lst.append(i)
+#for i in range (fps_bottom,fps_top+1,fps_increment):
+#    fps_lst.append(i)
 
 ##Looping though ISO:
 
-iso_top=800
-iso_bottom=100
-iso_increment=100
-iso_lst=[]
-j=iso_bottom
+#iso_top=800
+#iso_bottom=100
+#iso_increment=100
+#iso_lst=[]
+#j=iso_bottom
 
-for j in range (iso_bottom,iso_top+1,iso_increment):
-    iso_lst.append(j)        
+#for j in range (iso_bottom,iso_top+1,iso_increment):
+#    iso_lst.append(j)        
 
-
-      
-###Testing to see if the below code can de removed:
-
-
-##Combinding both lists to get all possible permutations
-##Total permutations saved on total_per
-#combo=[]
-#total_per=0
-
-#for a in fps_lst:              ##for a variable (a) in list 1
-#    for b in iso_lst:          ##for a variable (b) in list 2
-#        combo.append([a,b])    ##append variables a and b into list called combo
-#        total_per=total_per+1
-        
-        
-##Making an array called permu_array and placing it in a list       
-#permu_array=np.array(combo)
-#permu_array=combo
-
-##Image naming using for loop
-
-#for i in range(total_per):
-#    condition=permu_array[i]
-#    fps=condition[0]
-#    iso=condition[1]
-    
 
 ##Camera Functions
 
@@ -84,10 +63,10 @@ def picture(fr,iso):
 	pictime = datetime.now().strftime('%Y_%m_%d_%H-%M-%S.%f')[:-4] ##Format for naming images
 	#picTime = time.ctime()
 	#t = str(picTime)
-	time.sleep(7) ##upon start preview, time in seconds of sleep before adjusting shutter speed    
+	camera.capture('/home/pi/Documents/Test_Camera/Test_Pics/%s_FR%s_ISO%s.jpg' %(pictime,fr,iso)) ##Images captured will be stored in this path
+	time.sleep(2) ##upon start preview, time in seconds of sleep before adjusting shutter speed    
 	camera.shutter_speed = 4000
         time.sleep(3) ##post adjusting shutterspeed, time in secs. sleep before capturing photo
-	camera.capture('/home/pi/Documents/Test_Camera/Test_Pics/%s_FR%s_ISO%s.jpg' %(pictime,fr,iso)) ##Images captured will be stored in this path
 	time.sleep(2) ## seconds of sleep after photo is captured before stop preview
 	camera.stop_preview() ##Display showing what is in camera frame closes.
 			      ##This programs will loop until all photos are taken.
@@ -115,9 +94,13 @@ if __name__ == '__main__':
 #	time.sleep(2)
 	for i in fps_lst:
     		for j in iso_lst:  
+			picture(i,j)
+			picture(i,j)
+			picture(i,j)
+			picture(i.j) 
 			picture(i,j) ##Calling picture() function to cycle through all FR and ISO
 
-	off() ##Caling off() function
+	off() ##Calling off() function
 	#time.sleep(5)
 	#print(pictime)
 
